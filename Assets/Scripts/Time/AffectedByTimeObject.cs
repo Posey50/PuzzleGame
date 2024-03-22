f1b448d3819048146df4e6f4fe0f58a3d5a6101c
Posey50 <leolecointre50@gmail.com>
2024-03-22 16:53:57 +0100

    Changements d'influence du temps

--- Assets/Scripts/Time/AffectedByTimeObject.cs
+++ Assets/Scripts/Time/AffectedByTimeObject.cs
@@ -7,18 +7,22 @@ public class ObjectAffectedByTime : MonoBehaviour
     /// </summary>
     private Animator _animator;
 
+    /// <summary>
+    /// Object which will be affected.
+    /// </summary>
+    [field : SerializeField]
+    public MeshRenderer ObjectAffected {  get; private set; }
+
     void Start()
     {
         _animator = GetComponent<Animator>();
-
-        TimeManager.Instance.TimeChanged += SetSpeed;
     }
 
     /// <summary>
     /// Called when time scale is changed to update the speed of the animation.
     /// </summary>
     /// <param name="newSpeed"> New speed to set. </param>
-    private void SetSpeed(float newSpeed)
+    public void SetSpeed(float newSpeed)
     {
         _animator.SetFloat("TimeScale", newSpeed);
     }
--- Assets/Scripts/Time/TimeManager.cs
+++ Assets/Scripts/Time/TimeManager.cs
@@ -1,5 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.UI.ScrollRect;
 
 public class TimeManager : MonoBehaviour
 {
@@ -8,11 +12,7 @@ public class TimeManager : MonoBehaviour
 
     public static TimeManager Instance => _instance;
 
-    /// <summary>
-    /// Time scale used by the environment.
-    /// </summary>
-    public float TimeScale {  get; private set; }
-
+    [Header("Slider")]
     /// <summary>
     /// Minimum of the time scale.
     /// </summary>
@@ -28,7 +28,44 @@ public class TimeManager : MonoBehaviour
     /// <summary>
     /// Slider to control the time scale.
     /// </summary>
+    [SerializeField]
     private Slider _slider;
+
+    /// <summary>
+    /// Time scale used by the environment.
+    /// </summary>
+    private float _timeScale;
+
+    [Header("Level")]
+    /// <summary>
+    /// Comportement of the time manager.
+    /// </summary>
+    [SerializeField]
+    private TimeType _timeType;
+
+    /// <summary>
+    /// Delay before a changes.
+    /// </summary>
+    [SerializeField]
+    private float _delayBetweenChanges;
+
+    /// <summary>
+    /// Materials for moving objects.
+    /// </summary>
+    [SerializeField]
+    private Material _goldMaterial, _diamondMaterial;
+
+    /// <summary>
+    /// List of all objects affected by time.
+    /// </summary>
+    [SerializeField]
+    private List<ObjectAffectedByTime> _objectsAffectedByTime;
+
+    /// <summary>
+    /// List of all objects not affected by time, only necessary in Alternate mode.
+    /// </summary>
+    [SerializeField]
+    private List<ObjectAffectedByTime> _objectsNotAffectedByTime;
 
     /// <summary>
     /// Event to indicate the new time scale to all objects who depend on it.
@@ -36,6 +73,13 @@ public class TimeManager : MonoBehaviour
     /// <param name="timeScale"> New time scale. </param>
     public delegate void TimeDelegate(float timeScale);
     public event TimeDelegate TimeChanged;
+
+    /// <summary>
+    /// Event to indicate that the time influence has changed.
+    /// </summary>
+    /// <param name="timeScale"> New time scale. </param>
+    public delegate void InfluenceDelegate();
+    public event InfluenceDelegate InfluenceChanged;
 
     private void Awake()
     {
@@ -53,11 +97,77 @@ public class TimeManager : MonoBehaviour
 
     private void Start()
     {
-        _slider = GetComponent<Slider>();
+        _timeScale = 1f;
 
-        TimeScale = 1f;
+        switch (_timeType.ToString())
+        {
+            case "Constant":
+                for (int i = 0; i < _objectsAffectedByTime.Count; i++)
+                {
+                    TimeChanged += _objectsAffectedByTime[i].SetSpeed;
+                    _objectsAffectedByTime[i].ObjectAffected.material = _goldMaterial;
+                }
+                break;
+            case "Alternate":
+                for (int i = 0; i < _objectsAffectedByTime.Count; i++)
+                {
+                    TimeChanged += _objectsAffectedByTime[i].SetSpeed;
+                    _objectsAffectedByTime[i].ObjectAffected.material = _goldMaterial;
+                }
+                for (int i = 0; i < _objectsNotAffectedByTime.Count; i++)
+                {
+                    _objectsNotAffectedByTime[i].GetComponent<ObjectAffectedByTime>().SetSpeed(1f);
+                    _objectsNotAffectedByTime[i].ObjectAffected.material = _diamondMaterial;
+                }
+
+                StartCoroutine(TimeAlternate());
+                break;
+        }
 
         SetSlider();
+    }
+
+    /// <summary>
+    /// Called to wait before a changes.s
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator TimeAlternate()
+    {
+        yield return new WaitForSeconds(_delayBetweenChanges);
+
+        Alternate();
+    }
+
+    /// <summary>
+    /// Called to change the influence of the time.
+    /// </summary>
+    private void Alternate()
+    {
+        // Stocks objects affected by time temporary
+        List<ObjectAffectedByTime> temp = new();
+        temp.AddRange(_objectsAffectedByTime);
+        _objectsAffectedByTime.Clear();
+
+        // Reverses lists
+        _objectsAffectedByTime.AddRange(_objectsNotAffectedByTime);
+        _objectsNotAffectedByTime.Clear();
+        _objectsNotAffectedByTime.AddRange(temp);
+
+        // Changes materials and influences
+        for (int i = 0; i < _objectsAffectedByTime.Count; i++)
+        {
+            TimeChanged += _objectsAffectedByTime[i].SetSpeed;
+            _objectsAffectedByTime[i].GetComponent<ObjectAffectedByTime>().SetSpeed(_timeScale);
+            _objectsAffectedByTime[i].ObjectAffected.material = _goldMaterial;
+        }
+        for (int i = 0; i < _objectsNotAffectedByTime.Count; i++)
+        {
+            TimeChanged -= _objectsNotAffectedByTime[i].SetSpeed;
+            _objectsNotAffectedByTime[i].GetComponent<ObjectAffectedByTime>().SetSpeed(1f);
+            _objectsNotAffectedByTime[i].ObjectAffected.material = _diamondMaterial;
+        }
+
+        StartCoroutine(TimeAlternate());
     }
 
     /// <summary>
@@ -67,7 +177,7 @@ public class TimeManager : MonoBehaviour
     {
         _slider.minValue = _minRange;
         _slider.maxValue = _maxRange;
-        _slider.value = TimeScale * 2f;
+        _slider.value = _timeScale * 2f;
         _slider.onValueChanged.AddListener(OnSliderValueChanged);
         OnSliderValueChanged(_slider.value);
     }
@@ -78,7 +188,7 @@ public class TimeManager : MonoBehaviour
     /// <param name="value"> The new value of the slider. </param>
     private void OnSliderValueChanged(float value)
     {
-        TimeScale = _slider.value / 2f;
-        TimeChanged?.Invoke(TimeScale);
+        _timeScale = _slider.value / 2f;
+        TimeChanged?.Invoke(_timeScale);
     }
 }
