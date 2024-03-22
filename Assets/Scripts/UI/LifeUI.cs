1df621a165b89dfec26d9e8631753f9390526256
Posey50 <leolecointre50@gmail.com>
2024-03-22 15:18:42 +0100

    Ajout de l'indicateur de vies

---
+++ Assets/Scripts/UI/LifeUI.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class LifeUI : MonoBehaviour
+{
+    /// <summary>
+    /// Text which shows remaining lifes.
+    /// </summary>
+    private TMP_Text _remainingLifes;
+
+    private void Start()
+    {
+        _remainingLifes = GetComponent<TMP_Text>();
+        LvlManager.Instance.NewNumberOfLifes += UpdateUI;
+    }
+
+    /// <summary>
+    /// Called to update the number of lifes on screen.
+    /// </summary>
+    /// <param name="newNumberOfLifes"> New number of lifes to update. </param>
+    private void UpdateUI(int newNumberOfLifes)
+    {
+        _remainingLifes.SetText("x" + newNumberOfLifes.ToString());
+    }
+}
